974be42535efc6eed37a2d492ca30467c90882ec
Pavlo Tishynin <pashatishinin@gmail.com>
2026-04-12 21:15:08 +0200

    init

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,19 @@
+//background
+
+export { default as FluidScene } from "./animation/FluidScene.astro";
+
+//animations
+
+export { default as TextEffectLine } from "./animation/TextEffectLine.astro";
+export { default as TextEffect } from "./animation/TextEffect.astro";
+
+//common
+
+export { default as Circle } from "./common/Circle.astro";
+export { default as PageTransition } from "./common/PageTransition.astro";
+export { default as PreLoader } from "./common/PreLoader.astro";
+
+// buttons
+
+export { default as CtaButton } from "./ui/buttons/CtaButton.astro";
+export { default as LinkButton } from "./ui/buttons/LinkButton.astro";
---
+++ frontend/src/views/home/index.ts
@@ -0,0 +1,4 @@
+export { default as HeroSection } from "./hero/HeroSection.astro";
+export { default as PreviewSection } from "./preview/PreviewSection.astro";
+export { default as AchievementSection } from "./achievement/AchievementSection.astro";
+export { default as SelectedWorksSection } from "./selected-works/SelectedWorksSection.astro";
