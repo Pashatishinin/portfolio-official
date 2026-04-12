f1f5aa2270f07237c907fa6c1c2930a0a0df99ed
Pavlo Tishynin <pashatishinin@gmail.com>
2026-04-12 21:15:08 +0200

    init

---
+++ frontend/src/shared/data.ts
@@ -0,0 +1,23 @@
+export const slides = [
+  {
+    slideImg: "/1.jpg",
+    slideTitle: "Project One",
+    slideDescription: "Description for the first project.",
+    slideUrl: "#",
+    slideTags: ["Design", "Development"],
+  },
+  {
+    slideImg: "/1.jpg",
+    slideTitle: "Project Two",
+    slideDescription: "Description for the first project.",
+    slideUrl: "#",
+    slideTags: ["Design", "Development"],
+  },
+  {
+    slideImg: "/1.jpg",
+    slideTitle: "Project Three",
+    slideDescription: "Description for the first project.",
+    slideUrl: "#",
+    slideTags: ["Design", "Development"],
+  },
+];
---
+++ frontend/src/views/project/index.ts
@@ -0,0 +1,4 @@
+export { default as HeroSection } from "./hero/HeroSection.astro";
+export { default as AboutProjectSection } from "./about-project/AboutProjectSection.astro";
+export { default as NextProjectSection } from "./next-project/NextProjectSection.astro";
+export { default as ShortPreviewSection } from "./short-preview/ShortPreviewSection.astro";
