7cad1198b4de5a38a120afd2371627f12bd39352
Pavlo Tishynin <pashatishinin@gmail.com>
2026-04-27 13:53:00 +0200

    fluid

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   build: {
     inlineStylesheets: "always",
   },
+
+  integrations: [react()],
 });
