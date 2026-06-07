57672601debd4faa8b6c7628e5f728bf053f7644
Pavlo Tishynin <pashatishinin@gmail.com>
2026-06-07 21:33:37 +0200

    new

---
+++ frontend/src/shared/data/projects.data.ts
@@ -0,0 +1,33 @@
+export interface Project {
+	playbackId: string;
+	name: string;
+	link: string;
+	img: string;
+}
+
+export const projects: Project[] = [
+	{
+		playbackId: "T4nv3Umldrgdtzz4fyGenmWkUaCG023SDEJ5Y2ZsAyTA",
+		name: "Benjamin",
+		link: "/8enjamin",
+		img: "/benjamin-cover.jpg",
+	},
+	{
+		playbackId: "ysb02vT4Q1UV51n1m1ZslNwxo1HF01naI7q01P84x00Gn6o",
+		name: "Vettar",
+		link: "/vettar",
+		img: "/vettar-cover.jpg",
+	},
+	{
+		playbackId: "6WXxP01qeOXjJdclX00JOWiov5T5aBXb3nPGm02mnjZpl8",
+		name: "Ahin Anburaja",
+		link: "/ahin-anburaja",
+		img: "/ahin-cover.jpg",
+	},
+	{
+		playbackId: "eCuQkz01oCaMQgHw02tIQnN701dnSjCEViEdHh4S6vKN2k",
+		name: "Blive Coaching",
+		link: "/blive-coaching",
+		img: "/blive-cover.jpg",
+	},
+];
--- frontend/src/views/home/index.ts
+++ frontend/src/views/home/index.ts
@@ -1,4 +1,5 @@
+export { default as AchievementSection } from "./achievement/AchievementSection.astro";
 export { default as HeroSection } from "./hero/HeroSection.astro";
 export { default as PreviewSection } from "./preview/PreviewSection.astro";
-export { default as AchievementSection } from "./achievement/AchievementSection.astro";
+export { default as MyWorks } from "./selected-works/MyWorks.astro";
 export { default as SelectedWorksSection } from "./selected-works/SelectedWorksSection.astro";
